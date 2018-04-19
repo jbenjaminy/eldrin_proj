2df55c17de1d449bf7e81efc8a6881fb72c673b3
jbenjaminy <young.benjamin@gmail.com>
2018-04-19 01:17:30 -0500

    updates .gitignore, adds server-side function files

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const register = require('./firebase/functions/register');
 const login = require('./firebase/functions/login');
-const addRestaurant = require('./firebase/functions/addRestaurant');
-const findRestaurants = require('./firebase/functions/findRestaurants');
-const getDetails = require('./firebase/functions/findRestaurants');
+const addRestaurant = require('./firebase/functions/add-restaurant');
+const findRestaurants = require('./firebase/functions/find-restaurants');
+const getDetails = require('./firebase/functions/find-restaurants');
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -22,24 +23,70 @@ app.use((req, res, next) => {
 
 app.post('/register', (req, res) => {
     const credentials = req.body;
-    register(credentials).then((err, user) => {
-        if (err) {
-            console.error(err);
-            return res.sendStatus(500);
-        }
-        res.json({ user });
-    });
+    register(credentials)
+        .then((error, user) => {
+
+            if (error) {
+                console.error(error);
+                return res.sendStatus(500);
+            }
+
+            res.json({ user });
+        });
 });
 
 app.get('/login', (req, res) => {
     const credentials = req.body;
-    login(credentials).then((err, user) => {
-        if (err) {
-            console.error(err);
-            return res.sendStatus(500);
-        }
-        res.json({ user });
-    });
+    login(credentials)
+        .then((error, user) => {
+            if (error) {
+                console.error(error);
+                return res.sendStatus(500);
+            }
+
+            res.json({ user });
+        });
+});
+
+app.get('/add-restaurant', (req, res) => {
+
+    const restaurantDetails = req.body;
+
+    addRestaurant(restaurantDetails)
+        .then((details, error) => {
+            if (error) {
+                console.error(error);
+                return res.sendStatus(500);
+            }
+
+            res.json({ details });
+        });
+});
+
+app.get('/find-restaurants', (req, res) => {
+
+    findRestaurants(credentials)
+        .then((error, restaurants) => {
+            if (error) {
+                console.error(error);
+                return res.sendStatus(500);
+            }
+
+            res.json({ restaurants });
+        });
+});
+
+app.get('/get-details', (req, res) => {
+    const restaurantId = req.body;
+    getDetails(restaurantId)
+        .then((error, details) => {
+            if (error) {
+                console.error(error);
+                return res.sendStatus(500);
+            }
+
+            res.json({ details });
+        });
 });
 
 function runServer() {
