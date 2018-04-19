17f5ccef61aeeeb32d46a2715f68c980a1f051ca
jbenjaminy <young.benjamin@gmail.com>
2018-04-18 23:36:39 -0500

    adds project structure

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const register = require('./firebase/functions/register');
+const login = require('./firebase/functions/login');
+const addRestaurant = require('./firebase/functions/addRestaurant');
+const findRestaurants = require('./firebase/functions/findRestaurants');
+const getDetails = require('./firebase/functions/findRestaurants');
+
+const app = express();
+const jsonParser = bodyParser.json();
+const HOST = process.env.HOST;
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('build'));
+app.use(jsonParser);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.post('/register', (req, res) => {
+    const credentials = req.body;
+    register(credentials).then((err, user) => {
+        if (err) {
+            console.error(err);
+            return res.sendStatus(500);
+        }
+        res.json({ user });
+    });
+});
+
+app.get('/login', (req, res) => {
+    const credentials = req.body;
+    login(credentials).then((err, user) => {
+        if (err) {
+            console.error(err);
+            return res.sendStatus(500);
+        }
+        res.json({ user });
+    });
+});
+
+function runServer() {
+	app.listen(PORT, HOST, err => {
+		if (err) return console.error(err);
+		console.log(`Listening on port ${PORT}`);
+	});
+}
+
+if (require.main === module) runServer();
