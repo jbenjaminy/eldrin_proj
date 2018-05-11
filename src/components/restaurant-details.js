6386832fa3a39e40c67f9eed444a8763472a44ea
jbenjaminy <young.benjamin@gmail.com>
2018-05-10 22:11:41 -0500

    updates restaurant details styling, updates results images

--- src/components/restaurant-details.js
+++ src/components/restaurant-details.js
@@ -36,17 +36,9 @@ class RestaurantDetails extends Component {
             .join('-');
 
         const availableImages = [
-            'cherrys-pansiteria',
-            'fefang-pancitera',
-            'gretchens-noodle-haus',
-            'jomars-panciteria',
-            'nangs-panciteria',
-            'triple-j-panciteria'
+            'fefang-pancitera'
         ];
-        
-        const urls = {
-            'fefang-pancitera': 'https://www.google.com/maps/place/25+Lecaros+St,+Tuguegarao,+Cagayan,+Philippines/@17.6091626,121.7241511,17z/data=!3m1!4b1!4m5!3m4!1s0x3385842f40c919f9:0x5e48fb2aa3b4aa!8m2!3d17.6091626!4d121.7263451',
-        };
+
 
         let imageSrc = 'generic-thumbnail';
 
@@ -56,21 +48,34 @@ class RestaurantDetails extends Component {
             }
         });
 
-        const mapUrl = urls[imageSrc];
+        const urls = {
+            'fefang-pancitera': {
+                map: 'https://www.google.com/maps/place/25+Lecaros+St,+Tuguegarao,+Cagayan,+Philippines/@17.6091626,121.7241511,17z/data=!3m1!4b1!4m5!3m4!1s0x3385842f40c919f9:0x5e48fb2aa3b4aa!8m2!3d17.6091626!4d121.7263451',
+                thumbnail: 'http://i.imgur.com/Q708JxG.png'
+            }
+        };
+
+        const mapUrl = urls[imageSrc].map;
+        const thumbnailUrl = urls[imageSrc].thumbnail;
 
         return (
             <div className='results-container'>
                 <div className='modal result-item details'>
-                    <div className='buffer' />
-                    <h2 className='no-hover-effect'>{name}</h2>
+                    <div className='result-col col-left'>
+                        <div className='buffer' />
 
-                    <a href={mapUrl}><img
-                        src={`${imageSrc}-map.jpg`}
-                        alt={`${name}`}
-                        className='result-image result-col'
-                    /></a>
+                        <h2 className='no-hover-effect'>{name}</h2>
+
+                        <a href={mapUrl}><img
+                            src={thumbnailUrl}
+                            alt={`${name}`}
+                            className='result-image-lg'
+                        /></a>
+                    </div>
+
 					<div className='result-col'>
                         <h3 className='subheader'>Menu</h3>
+
                         <ul>
                             <li className='menu-item'>"Regular"<span className='menu-item-price'> - 60 PHP</span></li>
                             <li className='menu-item'>"Special"<span className='menu-item-price'> - 70 PHP</span></li>
@@ -79,15 +84,20 @@ class RestaurantDetails extends Component {
 					</div>
 
 					<div className='result-col distance-col'>
+
+                        <div className='buffer-lg' />
+
                         <h3 className='subheader margin'>Location</h3>
+
                         <ul className='output'>
                             <li className='item'><p className='result-spec'>{address}</p></li>
                             <li className='item'><p className='result-spec'>{city}, {region}, {country}</p></li>
                         </ul>
+
                         <ul className='output'>
-                            <li className='item'><p className='result-spec neighborhood duration'>{neighborhood}</p></li>
-                            <li className='item'><p className='result-spec neighborhood duration'>{distance}</p></li>
-                            <li className='item'><p className='result-spec neighborhood duration'>{duration} driving</p></li>
+                            <li className='item'><p className='result-spec neighborhood'>{neighborhood}</p></li>
+                            <li className='item'><p className='result-spec neighborhood'>{distance}</p></li>
+                            <li className='item'><p className='result-spec neighborhood'>{duration} driving</p></li>
                         </ul>
 					</div>
 				</div>
